0653f2c6cc8d791cc9912d213119dd5f42efb605
rodrigo-projects <rodrigorrsousa6@gmail.com>
2021-02-08 23:22:11 -0300

    0

--- Backend/api/teste01.py
+++ Backend/api/teste01.py
@@ -1,23 +1,23 @@
-import RPi.GPIO as gp
-import time
+# import RPi.GPIO as gp
+# import time
 import sys
 
-gp.setwarnings(False)
-gp.setmode(gp.BCM)
+# gp.setwarnings(False)
+# gp.setmode(gp.BCM)
 
-gp.setup(13,gp.OUT)
+# gp.setup(13,gp.OUT)
 
 a=0
 b=100
 p=0.0025
 a=sys.argv[1]
 print(a)
-while (a<100):
-   gp.output(13,gp.HIGH)
-   time.sleep(p)
-   gp.output(13,gp.LOW)
-   time.sleep(p)
-   a=a+1
+# while (a<100):
+#    gp.output(13,gp.HIGH)
+#    time.sleep(p)
+#    gp.output(13,gp.LOW)
+#    time.sleep(p)
+#    a=a+1
     
-gp.cleanup()
+# gp.cleanup()
 print("fim")
