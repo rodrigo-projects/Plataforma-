8caf9c0eb6e3928b39a3f66b1f0a8565dcaabfd5
rodrigo-projects <rodrigorrsousa6@gmail.com>
2021-02-08 23:32:11 -0300

    0

--- Backend/arduino/teste01.py
+++ Backend/arduino/teste01.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as gp
 import time
-
+import sys
 
 gp.setwarnings(False)
 gp.setmode(gp.BCM)
@@ -10,8 +10,8 @@ gp.setup(13,gp.OUT)
 a=0
 b=100
 p=0.5
-
-print(a)
+r=sys.argv[1]
+print(r)
 while (a<100):
    gp.output(13,gp.HIGH)
    time.sleep(p)
